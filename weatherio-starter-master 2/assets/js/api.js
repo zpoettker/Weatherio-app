21f166acdc5f91389afca762e47c77ae247e785d
zpoettker <poettkerzach@gmail.com>
2024-11-02 20:08:50 +0000

    1.44.5

--- weatherio-starter-master 2/assets/js/api.js
+++ weatherio-starter-master 2/assets/js/api.js
@@ -4,3 +4,9 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+'use strict';
+
+const api_key = "9b9c336ba9a48ed62d7793fa192ee7c7";
+
+export const fetchData = function(URL. callback)
