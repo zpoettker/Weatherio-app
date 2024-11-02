7de73ca32725241588f3204f76807a0fb71fc880
zpoettker <poettkerzach@gmail.com>
2024-11-02 22:24:56 +0000

    1.52.07

--- weatherio-starter-master 2/assets/js/api.js
+++ weatherio-starter-master 2/assets/js/api.js
@@ -9,4 +9,37 @@
 
 const api_key = "9b9c336ba9a48ed62d7793fa192ee7c7";
 
-export const fetchData = function(URL. callback)
+/**
+ * Fetch data from server
+ * @param {string} URL  API url
+ * @param {Function} callback callback
+ */
+
+export const fetchData = function(URL, callback) {
+    fetch(`${URL}&appid=${api_key}`)
+    .then(res => res.json())
+    .then(data => callback(data));
+}
+
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon) {
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /**
+     * 
+     * @param {string} query Search query ex. "London", "New York"
+     * @returns 
+     */
+    geo(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
