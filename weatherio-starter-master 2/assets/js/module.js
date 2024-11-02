c72c45e89bdf619155f5062563364036e3576129
zpoettker <poettkerzach@gmail.com>
2024-11-02 22:30:05 +0000

    1.54

--- weatherio-starter-master 2/assets/js/module.js
+++ weatherio-starter-master 2/assets/js/module.js
@@ -4,3 +4,36 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+'use strict';
+
+export const weekDayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+export const monthName = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+];
+
+export const getDate = function(dataUnix, timezone) {
+    const date = new Date((dataUnix + timezone) * 1000);
+    const weekDayName = weekDayNames[date.getUTCDay()];
+    const monthName = monthNames
+}
