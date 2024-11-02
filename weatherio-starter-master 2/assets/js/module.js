f7463318a1c02aa5aa8264a08c9b2fcb75923412
zpoettker <poettkerzach@gmail.com>
2024-11-02 22:59:51 +0000

    2.02.15

--- weatherio-starter-master 2/assets/js/module.js
+++ weatherio-starter-master 2/assets/js/module.js
@@ -17,7 +17,7 @@ export const weekDayNames = [
     "Saturday"
 ];
 
-export const monthName = [
+export const monthNames = [
     "Jan",
     "Feb",
     "Mar",
@@ -32,8 +32,83 @@ export const monthName = [
     "Dec"
 ];
 
+/**
+ * 
+ * @param {number} dataUnix Unix date in seconds
+ * @param {number} timezone Timezone shift from UTC in seconds
+ * @returns {string} Data String. format: "Sunday 10, Jan"
+ */
+
 export const getDate = function(dataUnix, timezone) {
     const date = new Date((dataUnix + timezone) * 1000);
     const weekDayName = weekDayNames[date.getUTCDay()];
-    const monthName = monthNames
+    const monthName = monthNames[date.getUTCMonth()];
+
+    return `${weekDayName} ${date.getUTCDay()}, ${monthName}`;
 }
+
+/**
+ * 
+ * @param {number} timeUnix Unix date in seconds
+ * @param {number} timezone shift from UTC in seconds
+ * @returns {string} Time string. Format: "HH:MM AM/PM"
+ */
+
+export const getTime = function(timeUnix, timezone) {
+    const date = new Date((timeUnix + timezone) * 1000);
+    const hours = date.getUTCHours();
+    const minutes = date.getUTCMinutes();
+    const period = hours >= 12 ? "PM" : "AM";
+
+    return `${hours % 12 || 12}:${minutes} ${period}`;
+}
+
+/**
+ * 
+ * @param {number} timeUnix Unix date in seconds
+ * @param {number} timezone shift from UTC in seconds
+ * @returns {string} Time string. Format: "HH AM/PM"
+ */
+
+export const getHours = function(timeUnix, timezone) {
+    const date = new Date((timeUnix + timezone) * 1000);
+    const hours = date.getUTCHours();
+    const period = hours >= 12 ? "PM" : "AM";
+
+    return `${hours % 12 || 12} ${period}`;
+}
+
+/**
+ * 
+ * @param {number} mps Metter per seconds
+ * @returns {number} Kilometer per hours
+ */
+export const mps_to_kmh = mps => {
+    const mph = mps * 3600;
+    return mph / 1000;
+}
+
+export const aqiText = {
+    1: {
+        level: "Good",
+        message: "Air quality is considered satisfactory, and air pollution poses little or no risk"
+    },
+    2: {
+        level: "Fair",
+        message: "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."
+    },
+    3: {
+        level: "Moderate",
+        message: "Members of sensitive groups may experience health effects. The general public is not likely to be affected."
+    },
+    4: {
+        level: "Poor",
+        message: "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects"
+    },
+    5: {
+        level: "Very Poor",
+        message: "Health warnings of emergency conditions. The entire population is more liekly to be affected."
+    }
+}
+
+
