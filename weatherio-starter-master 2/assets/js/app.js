8002706ad0a39c23beae286b52b15fdf1b6240a5
zpoettker <poettkerzach@gmail.com>
2024-11-03 16:42:20 +0000

    2.13.0

--- weatherio-starter-master 2/assets/js/app.js
+++ weatherio-starter-master 2/assets/js/app.js
@@ -3,3 +3,29 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+'use strict';
+
+import {fetchData, url } from "./api.js";
+
+import * as module from "./module.js";
+
+/**
+ * Add event listener on multiple elements
+ * @param {Nodelist} elements Elements node array
+ * @param {string} eventType  Event Type ex. "click", "mouseover"
+ * @param {function} callback  Callback function
+ */
+const addEventOnElements = function (elements, eventType, callback) {
+    for (const element of elements) element.addEventListener(eventType, callback);
+}
+
+/**
+ * Toggle search in mobile devices
+ */
+
+const searchView = document.querySelector("[data-search-view]");
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+
+const toggleSearch = () => searchView.classList.toggle("active");
+addEventOnElements(searchTogglers, "click", toggleSearch);
